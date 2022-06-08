28d249c39ab1ea6a9dd9bdfa83a09a5b91de4260
Marco <marc-bachelet@orange.fr>
2022-06-08 17:40:48 +0200

    Ajout dossier models avec fichier User.js et Sauces.js

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://marco:marco@cluster0.mp0ey0t.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use((req, res, next) => {
-  console.log("requête reçue!");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ msg: "Bien reçu" });
-  next();
-});
-
-app.use((req, res) => {
-  console.log("Réponse envoyée avec succès");
-});
+// Permet de récupérer le body de la requête sans body parser
+app.use(express.json());
 
 module.exports = app;
