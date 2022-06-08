7dec5506402c339381a0ed7ac04afee6c4df3c82
Marco <marc-bachelet@orange.fr>
2022-06-08 15:30:38 +0200

    Premier commit:Fichier de base ok et Serveur node ok

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("requête reçue!");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ msg: "Bien reçu" });
+  next();
+});
+
+app.use((req, res) => {
+  console.log("Réponse envoyée avec succès");
+});
+
+module.exports = app;
