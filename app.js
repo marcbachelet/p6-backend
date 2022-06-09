992bca9fb19f3db9994b03653dab35e676c59931
Marco <marc-bachelet@orange.fr>
2022-06-09 17:43:30 +0200

    Création dossiers controllers, middleware, routes ainsi que leurs fichiers .js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
 
 const app = express();
+
+// connecte mongoose a mongodb
 mongoose
   .connect(
     "mongodb+srv://marco:marco@cluster0.mp0ey0t.mongodb.net/?retryWrites=true&w=majority",
@@ -12,5 +16,21 @@ mongoose
 
 // Permet de récupérer le body de la requête sans body parser
 app.use(express.json());
+
+// change les headers pour pouvoir éxécuter des requêtes
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
