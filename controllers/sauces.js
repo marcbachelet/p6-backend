dfe5207ba114b3b0fd3fef282ff58ac9fa21bba4
Marco <marc-bachelet@orange.fr>
2022-06-14 18:38:19 +0200

    Création router.post likeSauceOrNot pour système like et dislike. route ok, faire plus de tests

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -70,3 +70,91 @@ exports.getAllSauce = (req, res, next) => {
     .then((sauces) => res.status(200).json(sauces))
     .catch((error) => res.status(400).json({ error }));
 };
+
+// exports.likeSauceOrNot = (req, res, next) => {
+//   Sauce.findOne({ _id: req.params.id })
+//     .then((sauce) => {
+//       if (!sauce.usersLiked.includes(req.body.userId) && req.body.like === 1) {
+//         Sauce.updateOne(
+//           { _id: req.params.id },
+//           {
+//             $inc: { likes: 1 },
+//             $push: { usersLiked: req.body.userId },
+//           }
+//         )
+//           .then(() => res.status(201).json({ msg: "Sauce +1 like" }))
+//           .catch((error) => res.status(400).json({ error }));
+//       }
+//       if (Sauce.usersLiked.includes(req.body.userId) && req.body.like === 0) {
+//         Sauce.updateOne(
+//           { _id: req.params.id },
+//           {
+//             $inc: { likes: -1 },
+//             $pull: { usersLiked: req.body.userId },
+//           }
+//         )
+//           .then(() => res.status(201).json({ msg: "Sauce 0 like" }))
+//           .catch((error) => res.status(400).json({ error }));
+//       }
+//     })
+//     .catch((error) => res.status(400).json({ error }));
+// };
+
+exports.likeSauceOrNot = async (req, res, next) => {
+  try {
+    // charge la sauce en fonction de l'id dans la req.params
+    const sauce = await Sauce.findOne({ _id: req.params.id });
+    // Si user n'est pas dans le tableau usersLiked et qu'il aime la sauce
+    if (!sauce.usersLiked.includes(req.body.userId) && req.body.like === 1) {
+      await Sauce.updateOne(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+          $push: { usersLiked: req.body.userId },
+        }
+      );
+    }
+    // Si user est dans tableau usersLiked et qu'il annule son like
+    else if (
+      sauce.usersLiked.includes(req.body.userId) &&
+      req.body.like === 0
+    ) {
+      await Sauce.updateOne(
+        { _id: req.params.id },
+        {
+          $inc: { likes: -1 },
+          $pull: { usersLiked: req.body.userId },
+        }
+      );
+    }
+    // Si user n'est pas dans le tableau disLiked et qu'il n'aime pas la sauce
+    else if (
+      !sauce.usersDisliked.includes(req.body.userId) &&
+      req.body.like === -1
+    ) {
+      await Sauce.updateOne(
+        { _id: req.params.id },
+        {
+          $inc: { dislikes: 1 },
+          $push: { usersDisliked: req.body.userId },
+        }
+      );
+    }
+    // Si user est dans tableau disLiked et qu'il annule son disLike
+    else if (
+      sauce.usersDisliked.includes(req.body.userId) &&
+      req.body.like === 0
+    ) {
+      await Sauce.updateOne(
+        { _id: req.params.id },
+        {
+          $inc: { dislikes: -1 },
+          $pull: { usersDisliked: req.body.userId },
+        }
+      );
+    }
+    res.status(200).json({ message: "choix modifié" });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
